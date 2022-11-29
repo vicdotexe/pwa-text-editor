7a8e869d762a5351769f3abc30c8374aa1268b77
victor <vicdotexe@gmail.com>
2022-11-28 22:37:47 -0800

    indexedDB functionality working

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,19 +3,33 @@ import { openDB } from 'idb';
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('documents')) {
+        console.log('document database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('documents', { keyPath: 'id', autoIncrement: true });
+      console.log('document database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+
+  const db = await openDB('jate');
+  const tx = db.transaction('documents', 'readwrite');
+  const store = tx.objectStore('documents');
+  console.log('storing content', content)
+  await store.put({id:1, text:content});
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate');
+  const tx = db.transaction('documents', 'readonly');
+  const store = tx.objectStore('documents');
+  const result = await store.get(1);
+  console.log('getting result', result)
+  return result ? result.text : undefined;
+};
 
 initdb();
